1063f091ff77a2a8e44d06d4f5a0a08abd2bf444
test <test@test.test>
2014-11-15 12:22:16 +0200

    bug

--- flask_hell/app.py
+++ flask_hell/app.py
@@ -1,11 +1,33 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/yoyoyo")
+app.config["DEBUG"] = True
 
-def hell():
-	return "hell yo"
+@app.route("/integer/<int:value>")
+def int_type(value):
+	value += 2
+	print (value)
+	return "incorrect!!!!!"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print (value + 2)
+	return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print (value)
+	return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower()=="one":
+		return "onehell, {}".format(name), 200
+	else:
+		return "not found", 404
+
+
+
 
 if __name__ == "__main__":
 	app.run()
